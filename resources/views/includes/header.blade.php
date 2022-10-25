e6a5efe822f80b49c3520fe9bb2d58ad59771e69
Marczello00 <56977890+Marczello00@users.noreply.github.com>
2022-10-25 14:08:54 +0200

    Initial commit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,26 @@
+<div class="container">
+       <a id="logo" href="/">Serwis</a>
+
+
+       <nav class="navbar navbar-expand-xxl bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="">Strona główna</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="queue">Kolejka</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="pricing">Cennik</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
