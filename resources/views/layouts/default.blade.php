fcd9aab67727a69ab1de8389fa9bfa874f82ae00
Marczello00 <56977890+Marczello00@users.noreply.github.com>
2022-11-07 11:05:01 +0100

    modified:   resources/views/includes/head.blade.php 	modified:   resources/views/includes/header.blade.php 	modified:   resources/views/layouts/default.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,22 +1,23 @@
 <!doctype html>
 <html lang="en">
-<head>
-   @include('includes.head')
-</head>
-<body class="bg-dark" onload="checkCookieLanguage()">
-<div class="container">
-   <header class="row">
-       @include('includes.header')
-   </header>
-   </div>
-   <div id="main" class="row">
-           @yield('content')
-   </div>
-   <footer class="row">
-       @include('includes.footer')
-   </footer>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+<head>
+    @include('includes.head')
+</head>
+
+<body class="container" onload="checkCookieLanguage()">
+    <div class="container">
+        <header class="row">
+            @include('includes.header')
+        </header>
+    </div>
+    <div id="main" class="row">
+        @yield('content')
+    </div>
+    <footer class="row">
+        @include('includes.footer')
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
+
 </html>
