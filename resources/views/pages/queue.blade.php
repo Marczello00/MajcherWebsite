078acc224e8862eba73ca728067b058a6d65c088
Marczello00 <56977890+Marczello00@users.noreply.github.com>
2022-11-09 18:24:09 +0100

    modified:   resources/views/pages/queue.blade.php

--- resources/views/pages/queue.blade.php
+++ resources/views/pages/queue.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.default')
 @section('content')
    i am the queue page
+
+   <!-- Z tego poradnika można zrobić stream kamery kolejki bez 3rd party software
+      https://phoboslab.org/log/2013/09/html5-live-video-streaming-via-websockets
+   -->
 @stop
