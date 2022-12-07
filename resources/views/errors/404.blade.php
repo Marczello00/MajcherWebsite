07b6766109ae3556a0c0354cf19ca495211e98ac
Marczello00 <56977890+Marczello00@users.noreply.github.com>
2022-12-07 20:26:38 +0100

    new file:   resources/views/errors/404.blade.php Added 404 page. So simple!

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('content')
+<div class="d-flex justify-content-center text-light" style="margin-top: 5%; margin-bottom: 5%;">
+   <h2>Coś poszło nie tak...<br>Wróć na stronę główną.</h2></div>
+@stop
