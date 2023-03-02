7dabd39f530147cd6844914255d90fc79b657ee5
Marczello00 <56977890+Marczello00@users.noreply.github.com>
2023-03-02 21:00:01 +0100

    modified:   public/css/style.css 	modified:   public/js/scripts.js reapired hover effects on touch devices

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -57,6 +57,34 @@ function eraseCookie(name) {
         name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
 }
 
+function watchForHover() {
+    // lastTouchTime is used for ignoring emulated mousemove events
+    // that are fired after touchstart events. Since they're
+    // indistinguishable from real events, we use the fact that they're
+    // fired a few milliseconds after touchstart to filter them.
+    let lastTouchTime = 0
+  
+    function enableHover() {
+      if (new Date() - lastTouchTime < 500) return
+      document.body.classList.add('hasHover')
+    }
+  
+    function disableHover() {
+      document.body.classList.remove('hasHover')
+    }
+  
+    function updateLastTouchTime() {
+      lastTouchTime = new Date()
+    }
+  
+    document.addEventListener('touchstart', updateLastTouchTime, true)
+    document.addEventListener('touchstart', disableHover, true)
+    document.addEventListener('mousemove', enableHover, true)
+  
+    enableHover()
+  }
+
+
 function httpsGetJson(theUrl) {
     return fetch(theUrl, {
         method: "GET",
@@ -141,4 +169,5 @@ function check_cameras() {
 }
 document.addEventListener("DOMContentLoaded", function () {
     checkCookieLanguage();
+    watchForHover()
 });
