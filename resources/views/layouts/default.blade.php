d3e1d84a7b55d2d662314c227b215104adf2da6f
Marczello00 <56977890+Marczello00@users.noreply.github.com>
2023-05-09 21:17:34 +0200

    Adding ads

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -18,7 +18,7 @@
     <footer class="row">
         @include('includes.footer')
     </footer>
-
+    <script type="text/javascript"> var infolinks_pid = 3395374; var infolinks_wsid = 0; </script> <script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>
 </body>
 
 </html>
