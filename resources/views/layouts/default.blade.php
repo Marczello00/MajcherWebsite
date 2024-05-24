38d5ea66221e3083fa4319591a45f7e21cb5e902
Marcel Majcher <56977890+Marczello00@users.noreply.github.com>
2024-05-24 09:45:55 +0200

    Added GA4 tag

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,6 +4,15 @@
 <head>
     @include('includes.head')
 </head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6H7934L02X"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6H7934L02X');
+</script>
 
 <body id="mainBackground1" class="container-fluid bg-black" onload="" style="font-family: 'Signika Negative', sans-serif;">
 
